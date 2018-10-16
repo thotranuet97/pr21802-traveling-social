684844d5803f61d3b342d2cdbc9a499008059ba8
thotranuet9 <thotranuet97@gmail.com>
2018-10-16 10:57:44 +0700

    Add gem config, I18n

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module Prj2
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
-  devise_for :users
-  root 'page#index'
-  namespace :admin do
-    get "/", to: "dashboard#index"
+  scope "(:locale)", locale: /en|vi/ do
+    mount Ckeditor::Engine => '/ckeditor'
+    devise_for :users
+    root 'page#index'
+    namespace :admin do
+      get "/", to: "dashboard#index"
+    end
   end
 end
