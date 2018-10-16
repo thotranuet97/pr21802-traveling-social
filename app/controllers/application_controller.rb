6baf63626fc6e23d5b01ba817e86807bfcf1f864
hvdung14@gmail.com <hvdung14@gmail.com>
2018-10-16 15:24:46 +0700

    Create devise layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_locale
-  
+  layout :layout_by_resource
+
   private
+
+  def layout_by_resource
+    devise_controller? ? "devise" : "application"
+  end
+  
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
+  
   def default_url_options
     {locale: I18n.locale}
   end
